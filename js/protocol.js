d6b3cf942addb34c14b80ed786a1cf5391f56205
jayhung97724 <jayhung97724@gmail.com>
2018-10-16 04:16:03 +0800

    protocol finished

--- js/protocol.js
+++ js/protocol.js
@@ -1,24 +1,102 @@
-let protocols = [];
+let protocols = [
+  {
+    "title": "Cultivation of <i>Arabidopsis thaliana</i> seeds",
+    "content": [
+      "Use 3D Micro Shaker to shake seeds for 15 minutes with 20%/ bleach +1/1000 SDS (Tween20)",
+      "Remove bleach completely and add sterile water to wash three times",
+      "Storage at 4℃ for 4-7 days for Seed vernalization.",
+      "Pipette 100 μl of water and seeds to solid 1/2 M.S medium.",
+      "Seeds should be cultivated into plates in enough spaces to grow (Each plate should have 15-18 seeds)",
+      "Tilt plates and remove excess water",
+      "Seal the plates with parafilm",
+      "Put plates to growth chamber at constant temperature of 25 ± 2 ° C with 16/8 h light and dark cycle"
+    ]
+  },
+  {
+    "title": "Inoculation",
+    "content": [
+      "take 8-day plants",
+      "choose healthy plants which have complete roots ",
+      "Holding plant hypocotyls with tweezers and put plants in the cover of plate",
+      "Add proper amount bacteria of OD600=0.4 to cover whole roots and sink 1minutes.",
+      "Wash excess bacteria by sterile water for 1-2 second then put plants into new solid M.S medium. (For the test of how long the bacteria could stay in plants and the test of phenotype after inoculating)",
+      "Seal the plates with parafilm ",
+      "For endophytic test, do further experiment"
+    ]
+  },
+  {
+    "title": "Endophytic test",
+    "content": [
+      "sterilized tools: tweezers(label A、B)、dd\\(H_2O\\)、Glass Bead、Disposable pestle",
+      "prepared three plates of sterile water(label 1、2、3) and a plate of 10%bleach and a plate of 75% ethanol.",
+      "Before picking plants, sterile tweezers by alcohol burner or fuego basic then wait it to cool down.",
+      "Put 3 inoculated plants into 10% bleach at the same time, timing 1 minutes<br>Use tweezer A to take plants out the put back to 75% ethanol, timing 30 seconds<br>Use tweezer A to take plants out the put back to sterile water1, timing 30 seconds<br>Use tweezer B to take plants out the put back to sterile water2, timing 30 seconds<br>Use tweezer B to take plants out the put back to sterile water3, timing 30 seconds<br>Use tweezer B to take plants out the put to labeled eppendorf<br>Repeat above-mentioned steps for different OD600 but the same species.<br>Take 100ul of sterile water3 to LB medium as the control group",
+      "renew sterile water1、2、3、ethanol and bleach for different bacteria species experiment",
+      "After grinding plants by disposable pestle, use 200ul pipette to drop 100ul to LB medium",
+      "Pour glass beads then shake horizontally to make fluid spread",
+      "Pour glass beads out to waste bottle.",
+      "Growth bacteria in proper temperature and wait 12-16 hours."
+    ]
+  },
+  {
+    "title": "Cloning (3A and Gibson Assembly)",
+    "content": [
+      "PCR<div class=\"ui hidden header\"></div><table class=\"ui celled table\"><tbody><tr><td class=\"eight wide\">KOD plus polymerase</td><td class=\"eight wide\">1 μl</td></tr><tr><td>dNTP</td><td>2 μl</td></tr><tr><td>10XBuffer</td><td>2 μl</td></tr><tr><td>Forward Primer</td><td>1 μl</td></tr><tr><td>Reverse Primer</td><td>1 μl</td></tr><tr><td>\\(MgSO_4\\) </td><td>2 μl</td></tr><tr><td>DNA</td><td>1 μl </td></tr><tr><td>\\(H_2O\\)</td><td>10 μl</td></tr></tbody></table><div class=\"ui hidden header\"></div>",
+      "Restriction Enzyme Digestion<div class=\"ui hidden header\"></div><p>Restriction enzyme : EcoRI , XbaI , SpeI , PStI </p><table class=\"ui celled table\"><tbody><tr><td>NEB restriction enzyme</td><td rowspan=\"2\">(1μl enzyme can react with 1 μg DNA)</td></tr><tr><td>DNA</td></tr><tr><td>10x Cutsmart buffer</td><td>2 μl</td></tr><tr><td>Add \\(H_2O\\)  </td><td>to 20 μl</td></tr></tbody></table><div class=\"ui hidden header\"></div>",
+      "Gel extraction THERMO Gel extraction<ol><li>Excise gel slice containing the DNA fragment using a clean scalpel or razor blade</li><li>Put into Eppendorf (remember to keep the gel weight in 100-200mg)</li><li>Add equal volume of binding buffer , vortex </li><li>Incubate it until all the gel is dissolved in the buffer</li><li>Transfer all the solution to the purification column then centrifuge for 15,000rpm, 1min</li><li>Discard the flow-through and add 100μl binding buffer then centrifuge for 15,000rpm, 1min</li><li>Discard the flow through and add 700μl wash buffer then centrifuge for 15,000rpm, 1min</li><li>Discard the flow through and repeat (7) process</li><li>Discard the flow through and centrifuge for 15,000rpm, 7 min</li><li>Transfer the purification column to the 1.5ml eppendorf</li><li>Incubate it in 50-55℃ for 10 min</li><li>Add 20μl \\(H_2O\\) (prewarm) or Elution buffer into the center of the column and wait for 2-3 min</li><li>Centrifuge for 15,000rpm, 7 min</li><li>Use nanodrop to quantify the nucleic acids’ concentration </li></ol><div class=\"ui hidden header\"></div>",
+      "DNA clean up (after cutting, if the residue is in 5-8bp you can use DNA clean up) Genemark DNA clean up kit<ol><li>Add 3 volumes of binding buffer, mix completely</li><li>Transfer the solution into purification column, centrifuge for 1 min in top speed</li><li>Discard the flow-through and add 700μl of wash solution, centrifuge for 1 min in top speed</li><li>Discard the flow-through and repeat step (3)</li><li>Centrifuge for 7 min in top speed</li><li>Incubate in 50-55℃</li><li>Add rewarmed \\(H_2O\\) or elution buffer into the center of the column, wait for 2-3 min</li><li>Centrifuge for 7 min in top speed</li><li>Use nanodrop to quantify the nucleic acids’ concentration</li></ol><div class=\"ui hidden header\"></div>",
+      "Ligation Promega T4 ligase<ol><li>Calcμlate the vector and insert ‘s ratio $${ng\\ of\\ vector\\times\\ kb\\ size\\ of\\ insert\\times molar\\ ratio\\ of\\ insert\\over kb\\ size\\ of\\ vector\\ vector} = ng\\ of\\ insert$$vector : insert = 1 : 3 is quite optimal</li><li>Add 10X buffer</li><li>Add T4 ligase</li><li>Add \\(H_2O\\) till 20μl in total volume</li><li>Put it in 4℃ overnight or 22℃ for 3 hr</li></ol><div class=\"ui hidden header\"></div>",
+      "Transformation Use DH5alpha E.coli comercial competent cell <ol><li>Add 10μl DNA into 40 μl competent cell </li><li>Put it on the ice for 20-30min</li><li>42℃ heat shock</li><li>Put it back onto the ice for 3 min</li><li>Add 250 SOC medium and 37℃ incubate for 1 hr</li><li>Plate all of the transformation and equally distribute it onto a 10 cm LB agar plate containing the appropriate antibiotic</li><li>37℃ incubate overnight</li></ol><div class=\"ui hidden header\"></div>",
+      "Gibson assembly<ol> <li>2–3 Fragment Assembly DNA 0.02-0.5 pmols, Gibson Assembly Master Mix 10μl, Add \\(H_2O\\)  to 20μl in total volume </li><li>Incubate in 50℃ for 15 min when 2-3 fragment assembly, store on ice or in -20℃ for transformation</li></ol>"
+    ]
+  },
+  {
+    "title": "Protein Preparation and Extraction",
+    "content": [
+      "overnight culture of recombinant strains was inoculated into 500 mL of fresh LB medium at 37°C, 200 rpm",
+      "Until the optical density at 600 nm was 0.8 to 1.0, 1 mM IPTG was added, and the culture was allowed to grow for another 3 to 12 h.",
+      "500 mL cells were centrifuged (11,000 x g, 4 °C and 15 min).",
+      "The pellet was washed twice with (50mM Tris-Hcl , 0.3M Nacl pH 7.4), and centrifuged at 10000 x g, 4 °C for 10 min.",
+      "The cells were then re-suspended in 30 mL of the same buffer, and maintained at 0 °C for sonication.",
+      "Unbroken cells and cell wall materials were removed by centrifugation at 22000 x g, 4°C for 30 min",
+      "the supernatant was decanted and kept at 4 °C."
+    ]
+  },
+  {
+    "title": "protein purification(Immobilized Metal Affinity Chromatography)",
+    "content": [
+      "use the supernatant from protein preparation step for binding with cobalt column for 20 min",
+      "wash with wash buffer (50Mm Tris-Hcl, 0.3M Nacl pH7.4)",
+      "elut with elution buffer (50Mm Tris-Hcl, 0.3M Nacl , 0.5M imidazole pH7.4 )",
+      "run SDS-Page to check protein purity"
+    ]
+  },
+  {
+    "title": "Medium",
+    "content": [
+      "<b>Solid M.S medium:</b><br>4.4g/L MS Murashige and skoog Basal Medium (store at 4° C fridge), 1.5 % Sucrose, and 0.8% agar, use HCl or KOH to adjusted pH to 5.7. autoclaved at 120°C for 20 mins."
+    ]
+  }
+];
 let $accordion = $('.accordion');
-$.getJSON("./data/protocols.json", (file) => {
-  protocols = file;
-  console.log(protocols);
-  protocols.forEach((ptc) => {
-    let templateString = $('#articleTemplate').html();
-    let template = $(templateString);
-    title = template.siblings('.title');
-    content = template.siblings('.content');
-    ptitle = title.find('span');
-    ptitle.append($.parseHTML(ptc.title));
-    $accordion.append(title);
-    olTag = content.find('ol');
-    _content = "";
-    ptc.content.forEach(step => {
-      _content = _content + `<li>${step}</li>`
-    });
-    olTag.append(_content);
-    $accordion.append(content);
-  })
-  $('.accordion').accordion('open', 0);
-  $('#articleTemplate').remove()
-});
+// $.getJSON("./data/protocols.json", (file) => {
+//   protocols = file;
+console.log(protocols);
+protocols.forEach((ptc) => {
+  let templateString = $('#articleTemplate').html();
+  let template = $(templateString);
+  title = template.siblings('.title');
+  content = template.siblings('.content');
+  ptitle = title.find('span');
+  ptitle.append($.parseHTML(ptc.title));
+  $accordion.append(title);
+  olTag = content.find('ol');
+  _content = "";
+  ptc.content.forEach(step => {
+    _content = _content + `<li>${step}</li>`
+  });
+  olTag.append(_content);
+  $accordion.append(content);
+})
+$('.accordion').accordion('open', 0);
+$('#articleTemplate').remove()
